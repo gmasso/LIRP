edcac891463dd9dfbde8576347ea08efd0998a66
gmasso <guillaume.massonnet@imt-atlantique.fr>
2018-01-29 11:08:29 +0100

    Some solutions

---
+++ Solutions/lirp5cl2ci_2_split100.sol
@@ -0,0 +1,89 @@
+--------------------------------------------
+----- DEPOTS -----
+Open depots: 	2 	  Depots not selected: 	0 	  1 	  
+------  QUANTITY DELIVERED TO DEPOTS  -----
+Period 0:    -- 	 -- 	31.2663113894831	
+Period 1:    -- 	 -- 	0.0	
+Period 2:    -- 	 -- 	0.0	
+Period 3:    -- 	 -- 	0.0	
+Period 4:    -- 	 -- 	0.0	
+Period 5:    -- 	 -- 	0.0	
+Period 6:    -- 	 -- 	0.0	
+Period 7:    -- 	 -- 	0.0	
+Period 8:    -- 	 -- 	0.0	
+Period 9:    -- 	 -- 	0.0	
+------  QUANTITY DELIVERED FROM DEPOTS to CLIENTS  -----
+Period 0:   
+Depot 2: 	  5.651646681093864	5.8853570040259	7.039841132002923	6.381550297429356	6.307916274931055	
+
+Period 1:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 2:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 3:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 4:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 5:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 6:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 7:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 8:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+Period 9:   
+Depot 2: 	  0.0	0.0	0.0	0.0	0.0	
+
+------  INVENTORY AT DEPOTS  -----
+Initial inventory:    -- 	 -- 	0.0	
+Period 0:    -- 	 -- 	0.0	
+Period 1:    -- 	 -- 	0.0	
+Period 2:    -- 	 -- 	0.0	
+Period 3:    -- 	 -- 	0.0	
+Period 4:    -- 	 -- 	0.0	
+Period 5:    -- 	 -- 	0.0	
+Period 6:    -- 	 -- 	0.0	
+Period 7:    -- 	 -- 	0.0	
+Period 8:    -- 	 -- 	0.0	
+Period 9:    -- 	 -- 	0.0	
+------ INVENTORY AT CLIENTS  -----
+Initial inventory :0.0	0.0	0.0	0.0	0.0	
+Period 0:   5.0709301861509495	5.2947044751556795	6.345175431577276	5.769905488108073	5.653252651985693	
+Period 1:   4.522669785491422	4.679799105191288	5.64681060626019	5.125875477955025	5.025477351692	
+Period 2:   3.95453320971629	4.07714308228025	4.952697771634859	4.497948939541599	4.400626412102601	
+Period 3:   3.384875704513256	3.5014778611248434	4.236803568396033	3.8755081892709535	3.748674159895341	
+Period 4:   2.825649878476592	2.9244851843371245	3.5267235244593813	3.2086615448348073	3.1257713906827314	
+Period 5:   2.262929377662851	2.34392250559148	2.816574231079818	2.5296078570289957	2.4965994868316104	
+Period 6:   1.6985713709545154	1.7385770826234128	2.1267553320323587	1.9068965578663861	1.8687818502289408	
+Period 7:   1.1341350710251301	1.1597521447520942	1.4208342757516181	1.2584937555053322	1.2475855090699048	
+Period 8:   0.553517997086532	0.5828645405882645	0.7272615844175675	0.6224361968493578	0.6145753474884633	
+Period 9:   0.0	0.0	0.0	0.0	0.0	
+------  LIST OF ROUTES  -----
+Period 0:   
+ route 4 starting from depot 2 and visiting clients : 	0 	 1 	 2 	 3 	 4 	  (cost = 224.44173184328324) 	
+Period 1:   
+Period 2:   
+Period 3:   
+Period 4:   
+Period 5:   
+Period 6:   
+Period 7:   
+Period 8:   
+Period 9:   
+------  SET OF ROUTES  -----
+[[0],[],[],[],[],[],[],[],[],[]]
+-----  RECALCULATION OF THE OBJECTIVE FUNCTION  -----
+Fixed cost of opening depots: 100.0
+Cost of routes: 344.145196347127
+Inventory costs: 210.73027958290336
+Objective function recalculated: 654.8754759300303
+Resolution time : 21 milliseconds
